29aaaff08ddd7b0dde0211cb8a4741e5a3134651
Igor Bari <mucsi96@gmail.com>
2022-01-06 08:40:09 +0100

    create css module utils

--- packages/eslint-plugin-example/rules/noUndefCSSModuleClass.js
+++ packages/eslint-plugin-example/rules/noUndefCSSModuleClass.js
@@ -1,48 +1,22 @@
-const { readFileSync } = require('fs');
-const { resolve, dirname } = require('path');
-
-function getCSSModuleClassNames(path) {
-  const module = readFileSync(path, 'utf8');
-  return module.match(/(?<=\.)[_A-Za-z0-9\-]+(?=[^}]+{)/g);
-}
+const { getCSSModuleImportDetails } = require('../utils/cssModuleUtils');
 
 exports.create = (context) => {
   const modules = [];
 
   return {
     ImportDeclaration(node) {
-      if (
-        node.source.type !== 'Literal' ||
-        !node.source.value.endsWith('.module.css')
-      ) {
-        return;
-      }
+      const cssModuleImport = getCSSModuleImportDetails(context, node);
 
-      const defaultSpecifier = node.specifiers.find(
-        ({ type }) => type === 'ImportDefaultSpecifier'
-      );
-
-      if (!defaultSpecifier) {
-        return;
-      }
-
-      const { name: identifierName } = defaultSpecifier.local;
-      const fileName = resolve(
-        dirname(context.getFilename()),
-        node.source.value
-      );
-      try {
-        const classNames = getCSSModuleClassNames(fileName);
-
-        if (identifierName) {
-          modules.push({ identifierName, classNames });
-        }
-      } catch (error) {
-        context.report(node, `CSS Module "${node.source.value}" doesn't exist`);
+      if (cssModuleImport) {
+        modules.push(cssModuleImport);
       }
     },
     MemberExpression(node) {
-      if (node.object.type !== 'Identifier' || node.property.type !== 'Identifier' || node.computed) {
+      if (
+        node.object.type !== 'Identifier' ||
+        node.property.type !== 'Identifier' ||
+        node.computed
+      ) {
         return;
       }
 
---
+++ packages/eslint-plugin-example/utils/cssModuleUtils.js
@@ -0,0 +1,40 @@
+const { readFileSync } = require('fs');
+const { resolve, dirname } = require('path');
+
+function getCSSModuleClassNames(path) {
+    const module = readFileSync(path, 'utf8');
+    return module.match(/(?<=\.)[_A-Za-z0-9\-]+(?=[^}]+{)/g);
+  }
+  
+  function getCSSModuleImportDetails(context, node) {
+    if (
+      node.source.type !== 'Literal' ||
+      !node.source.value.endsWith('.module.css')
+    ) {
+      return;
+    }
+  
+    const defaultSpecifier = node.specifiers.find(
+      ({ type }) => type === 'ImportDefaultSpecifier'
+    );
+  
+    if (!defaultSpecifier) {
+      return;
+    }
+  
+    const { name: identifierName } = defaultSpecifier.local;
+    const fileName = resolve(dirname(context.getFilename()), node.source.value);
+    try {
+      const classNames = getCSSModuleClassNames(fileName);
+  
+      if (identifierName) {
+        return { identifierName, classNames };
+      }
+    } catch (error) {
+      context.report(node, `CSS Module "${node.source.value}" doesn't exist`);
+    }
+  }
+
+  module.exports ={
+    getCSSModuleImportDetails
+  }
